573520cf1cd365a569d2007e8e93cb3cc5b61f8a
joaomlg <joaomarcos1999jm@gmail.com>
2022-10-30 13:15:11 -0300

    img path fixed

--- object_detection.py
+++ object_detection.py
@@ -3,7 +3,7 @@ import torch
 # Download do YOLO e modelo
 model = torch.hub.load('WongKinYiu/yolov7', 'yolov7')
 
-img = "inference/gta_v_tunel_640.jpeg"
+img = "images/gta_v_tunel_640.jpeg"
 results = model(img)
 results.print()
 print(results.xyxy[0])
