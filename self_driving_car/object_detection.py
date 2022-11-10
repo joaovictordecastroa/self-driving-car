7843c1ca65e3d4db72b049c5ed3f4627cedfc705
pierresousa <pierrevssousa@gmail.com>
2022-11-10 19:43:52 -0300

    add label only on specific images

--- self_driving_car/object_detection.py
+++ self_driving_car/object_detection.py
@@ -2,6 +2,8 @@ import torch
 import cv2
 from helpers import plot_one_box
 
+obstacles_labels = ['person', 'bicycle', 'car', 'motorcycle', 'bus', 'truck']
+
 model = torch.hub.load('WongKinYiu/yolov7', 'yolov7')
 
 img_path = "images/gta_v_tunel_640.jpeg"
@@ -14,7 +16,8 @@ results.print()
 
 for *box, conf, cls in results.pred[0]:
   label = f'{results.names[int(cls)]} {conf:.2f}'
-  plot_one_box(box, image, label=label, line_thickness=1)
+  if results.names[int(cls)] in obstacles_labels:
+    plot_one_box(box, image, label=label, line_thickness=1)
 
 print(results.xyxy[0])
 
